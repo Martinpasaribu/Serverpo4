1086910b2599319b8469494481e32ecdbc6ae1b0
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2023-12-17 22:52:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import ProductRoute from "./routes/ProductRoute.js";
+dotenv.config();
+
+const app = express();
+
+app.use(cors(
+{
+    origin: ["https://client-ten-psi.vercel.app","https://www.postman.com/"],
+    methods: ["POST", "GET", "PATCH"], // Perubahan disini ke 'methods'
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(ProductRoute);
+
+app.use("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(process.env.APP_PORT, () => {
+    console.log('Server up and running...' + process.env.APP_PORT);
+});
